34b82444b805efc1488173df6885c12bf92006d4
lean <leanndro.garcia@gmail.com>
2022-12-15 04:13:56 -0300

    agregado de funciones

--- js/main.js
+++ js/main.js
@@ -14,6 +14,13 @@ const ordenarMayorMenor = () => {
   mostrarListaOrdenada();
 };
 
+const filtrarLista = (tipoFiltro, filtro) => {
+  const listaOrdenada = maestroProductos.filter((producto) => producto[tipoFiltro] == filtro);
+  console.log(listaOrdenada);
+};
+
+//filtrarLista();
+
 /*mostrar la lista de productos*/
 const mostrarListaOrdenada = () => {
   const listaOrdenada = maestroProductos.map((producto) => "ID: " + producto.id + "  Producto:" + producto.nombre + "  Precio: $" + producto.precio);
@@ -41,6 +48,8 @@ const comprarProductos = (listaDeProductos) => {
 
     otroProducto = confirm("¿Desea agregar otro producto al carrito?");
   } while (otroProducto);
+
+  confirmarCompra();
 };
 
 /*agrega producto al carrito y verifica si esta repetido*/
@@ -54,4 +63,65 @@ const agregarAlCarrito = (producto, productoId, productoCantidad) => {
   }
 };
 
-ordenarMayorMenor();
+const confirmarCompra = () => {
+  const resumenPedido = carritoUsuario.map((producto) => "- " + producto.nombre + " | Cantidad: " + producto.cantidad);
+
+  const confirmarCompra = confirm("Resumen de pedido:" + "\n\n" + resumenPedido.join("\n")) + "\n\n" + 'Para confirmar su pedido presione "Aceptar". Si desea modificar su pedido presione cancelar';
+
+  if (confirmarCompra) {
+    finalizarCompra(resumenPedido);
+  } else {
+    const productoAEliminar = prompt("Ingrese el ID del producto que desea eliminar");
+    eliminarProducto(productoAEliminar);
+  }
+};
+
+const eliminarProducto = (productoId) => {
+  carritoUsuario.forEach((producto, index) => {
+    if (producto.id.toLowerCase() === productoId.toLowerCase()) {
+      if (producto.cantidad > 1) {
+        producto.cantidad--;
+      } else {
+        carritoUsuario.splice(index, 1);
+      }
+    }
+  });
+  confirmarCompra();
+};
+
+const finalizarCompra = (listaDeProductos) => {
+  const cantidadTotal = carritoUsuario.reduce((acc, item) => acc + item.cantidad, 0);
+  const precioTotal = carritoUsuario.reduce((acc, item) => acc + item.precio * item.cantidad, 0);
+  alert("Detalle de compra:\n\n" + listaDeProductos.join("\n") + "\n\nTotal de productos: " + cantidadTotal + "\n\n¨Precio total: $" + precioTotal + "\n\n¡Gracias por su compra!");
+};
+
+const comprar = () => {
+  const ordenar = confirm('¿Desea ordenar o filtrar los productos?.Presione "Cancelar" si desea visualizar todos los productos a la vez.');
+  if (ordenar) {
+    let opcionOrden = prompt('Si desea ordenar por precio ingrese "1" o si desea filtrar por categoría o talle ingrese "2"');
+
+    if (opcionOrden == 1) {
+      let productosBaratos = confirm('Si desea ver los productos mas baratos primero presione "Aceptar"');
+      if (productosBaratos == true) {
+        ordenarMenorMayor();
+      } else {
+        ordenarMayorMenor();
+      }
+    }
+    if (opcionOrden == 2) {
+      const tipoFiltro = prompt("Ingrese 1 si desea filtrar por categoría o ingrese 2 si desea filtrar por talle");
+      if (tipoFiltro == 1) {
+        const filtro = prompt("Seleccione que tipo de bicicleta desea filtrar (MTB, Hibrida, Kids o Road)");
+        filtrarLista(tipoFiltro, filtro);
+      }
+      if (tipoFiltro == 2) {
+        const filtro = prompt("Seleccione que talle desea filtrar (XS,S,M,L o XL)");
+        filtrarLista(tipoFiltro, filtro);
+      }
+    }
+  } else {
+    mostrarListaOrdenada();
+  }
+};
+
+comprar();
--- js/stock.js
+++ js/stock.js
@@ -5,42 +5,42 @@ const maestroProductos = [
     nombre: "Mojave 5.0 R29 XS",
     categoria: "MTB",
     talle: "XS",
-    precio: 80000
+    precio: 80000,
   },
   {
     id: "mojaveS",
     nombre: "Mojave 5.0 R29 S",
     categoria: "MTB",
     talle: "S",
-    precio: 80000
+    precio: 80000,
   },
   {
     id: "mojaveM",
     nombre: "Mojave 5.0 R29 M",
     categoria: "MTB",
     talle: "M",
-    precio: 80000
+    precio: 80000,
   },
   {
     id: "mojaveL",
     nombre: "Mojave 5.0 R29 L",
     categoria: "MTB",
     talle: "L",
-    precio: 80000
+    precio: 80000,
   },
   {
     id: "mojaveXL",
     nombre: "Mojave 5.0 R29 XL",
     categoria: "MTB",
     talle: "XL",
-    precio: 80000
+    precio: 80000,
   },
   {
     id: "stradaXS",
     nombre: "Strada 1.0 XS",
     categoria: "Road",
     talle: "XS",
-    precio: 200000
+    precio: 200000,
   },
   {
     id: "stradaS",
@@ -48,119 +48,119 @@ const maestroProductos = [
     categoria: "Road",
     categoria: "",
     talle: "S",
-    precio: 200000
+    precio: 200000,
   },
   {
     id: "stradaM",
     nombre: "Strada 1.0 M",
     categoria: "Road",
     talle: "M",
-    precio: 200000
+    precio: 200000,
   },
   {
     id: "stradaL",
     nombre: "Strada 1.0 L",
     categoria: "Road",
     talle: "L",
-    precio: 200000
+    precio: 200000,
   },
   {
     id: "stradaXL",
     nombre: "Strada 1.0 XL",
     categoria: "Road",
     talle: "XL",
-    precio: 200000
+    precio: 200000,
   },
   {
     id: "stradaXL",
     nombre: "Strada 1.0 XL",
     categoria: "Road",
     talle: "XL",
-    precio: 200000
+    precio: 200000,
   },
   {
     id: "stradaXL",
     nombre: "Strada 1.0 XL",
     categoria: "Road",
     talle: "XL",
-    precio: 200000
+    precio: 200000,
   },
   {
     id: "urbanXS",
     nombre: "Urban 1.0 XS",
     categoria: "Hibrida",
     talle: "XS",
-    precio: 140000
+    precio: 140000,
   },
   {
     id: "urbanS",
     nombre: "Urban 1.0 S",
     categoria: "Hibrida",
     talle: "S",
-    precio: 140000
+    precio: 140000,
   },
   {
     id: "urbanM",
     nombre: "Urban 1.0 M",
     categoria: "Hibrida",
     talle: "M",
-    precio: 140000
+    precio: 140000,
   },
   {
     id: "urbanL",
     nombre: "Urban 1.0 L",
     categoria: "Hibrida",
     talle: "L",
-    precio: 140000
+    precio: 140000,
   },
   {
     id: "urbanXL",
     nombre: "Urban 1.0 XL",
     categoria: "Hibrida",
     talle: "XL",
-    precio: 140000
+    precio: 140000,
   },
   {
     id: "mxr12",
     nombre: "MX Rodado 12",
     categoria: "Kids",
     talle: "12",
-    precio: 40000
+    precio: 40000,
   },
   {
     id: "mxr14",
     nombre: "MX Rodado 14",
     categoria: "Kids",
     talle: "14",
-    precio: 40000
+    precio: 40000,
   },
   {
     id: "mxr16",
     nombre: "MX Rodado 16",
     categoria: "Kids",
     talle: "16",
-    precio: 40000
+    precio: 40000,
   },
   {
     id: "mxr18",
     nombre: "MX Rodado 18",
     categoria: "Kids",
     talle: "18",
-    precio: 50000
+    precio: 50000,
   },
   {
     id: "mxr20",
     nombre: "MX Rodado 20",
     categoria: "Kids",
     talle: "20",
-    precio: 50000
+    precio: 50000,
   },
   {
     id: "scoutr24",
     nombre: "Scout Rodado 24",
     categoria: "Kids",
     talle: "24",
-    precio: 60000
+    precio: 60000,
   },
   {
     id: "scoutr26",
@@ -168,6 +168,5 @@ const maestroProductos = [
     categoria: "Kids",
     talle: "26",
     precio: 60000,
-  }
+  },
 ];
-
